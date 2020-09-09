43f873088c7bff52198ad5a88ff84c9daeac500d
chayanne19 <chayannepaulino@gmail.com>
2020-09-08 23:10:27 -0700

    Running Merge Sort Algo

--- src/Algorithms/main.java
+++ src/Algorithms/main.java
@@ -4,5 +4,24 @@ public class main {
 
     public static void main(String[] args) {
 
+        MergeSort ms = new MergeSort();
+
+
+        int [] array = {5,2,7,1};
+
+        System.out.println("Un Sorted Array");
+        ms.printArray(array);
+
+        System.out.println();
+
+
+        System.out.println("Sorted Array");
+        array =  ms.mergeSort(array);
+        ms.printArray(array);
+
+
+
+
+
     }
 }
