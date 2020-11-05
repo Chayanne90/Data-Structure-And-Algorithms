8431e73d13c7846cac7cf23c3c5504583669eb86
chayanne19 <chayannepaulino@gmail.com>
2020-11-04 22:43:16 -0800

    Linked List Doublely

---
+++ src/DataStructure/LinkedList_Doubly.java
@@ -0,0 +1,72 @@
+package DataStructure;
+
+public class LinkedList_Doubly {
+
+    public Node first;
+    public  Node last;
+
+    /* Constructor */
+    public LinkedList_Doubly() { }
+
+    public boolean isEmpty() { return (first == null); }
+
+    public void insertFirst(Object data) {
+
+        Node newNode = new Node();
+        newNode.data = data;
+
+        if (isEmpty()) {
+            last = newNode;  /* if is empty, last will refer to the new node created */
+        } else {
+            first.previous = newNode;
+        }
+
+        newNode.next = first;
+        first = newNode;
+    }
+
+
+    public void insertLast(Object data ) {
+
+        Node newNode = new Node();
+        newNode.data = data;
+
+        if (isEmpty()) {
+            first = newNode;
+        } else {
+            last.next = newNode; /* assigning old last to newnode */
+            newNode.previous = last; /* the old last will be the newNode */
+        }
+        last = newNode;
+    }
+
+    /* assume if is not an empty list*/
+    public Node deleteFirst() {
+
+        Node temp = first;
+        if (first.next == null) {
+
+            last = null;
+        } else {
+
+            first.next.previous = first;
+        }
+        first = first.next;
+        return temp;
+    }
+
+   public Node deleteLast() {
+        
+   }
+
+
+
+
+
+
+
+
+
+
+
+}
