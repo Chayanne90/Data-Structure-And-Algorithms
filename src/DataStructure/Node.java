73a4227df0905aa3ad8c629cf2c99d72e80ea4d5
chayanne19 <chayannepaulino@gmail.com>
2020-11-17 22:26:41 -0800

    Node for Linked List

---
+++ src/DataStructure/Node.java
@@ -0,0 +1,17 @@
+package DataStructure;
+
+public class Node {
+    public Object data;
+    public Node next = null;
+    public Node previous = null;
+
+    public void displayNode() {
+        System.out.print("[ "+data+" ] -->");
+    }
+
+
+
+
+
+
+}
