f655d7fb6cc2dee0cfc4c63114036daf1a9bdbb6
chayanne19 <chayannepaulino@gmail.com>
2020-11-17 23:01:02 -0800

    BST Node

--- src/DataStructure/Node_BST.java
+++ src/DataStructure/Node_BST.java
@@ -6,11 +6,15 @@ public class Node_BST {
     Object value;
     Node_BST LeftChild, rightChild;
 
-
+    // Constructor
     public Node_BST(int key, Object value) {
 
         super();
         this.key = key;
         this.value = value;
     }
+
+
+
+
 }
