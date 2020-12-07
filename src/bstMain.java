7561be118911e2e0cd45323d85514cda46809937
chayanne19 <chayannepaulino@gmail.com>
2020-12-06 22:54:55 -0500

    run bst

---
+++ src/bstMain.java
@@ -0,0 +1,18 @@
+import DataStructure.BST;
+
+public class bstMain {
+
+    public static void main(String[] args) {
+    /*
+        BST bsTree = new BST();
+
+        bsTree.insert(10, "Ten");
+        bsTree.insert(20, "Twenty");
+        bsTree.insert(15, "fifteen");
+        bsTree.insert(100, "hundred");
+
+        System.out.println(bsTree.findMin().key);
+        System.out.println(bsTree.findMax().key);
+*/
+    }
+}
